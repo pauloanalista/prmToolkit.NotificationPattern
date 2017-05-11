6d5e6a3bb121c76a401a956e57bfc2c1f183252f
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2017-05-11 16:09:11 -0300

    Ajuste para se trabalhar com inversão de controle

--- prmToolkit.NotificationPattern/INotifiable.cs
+++ prmToolkit.NotificationPattern/INotifiable.cs
@@ -3,74 +3,48 @@ using System.Linq;
 
 namespace prmToolkit.NotificationPattern
 {
-    public abstract class Notifiable : INotifiable
+    public interface INotifiable
     {
-        private readonly List<Notification> _notifications;
-
-        protected Notifiable() { _notifications = new List<Notification>(); }
-
-        public IReadOnlyCollection<Notification> Notifications => _notifications;
+        IReadOnlyCollection<Notification> Notifications { get; }
 
         /// <summary>
         /// Adiciona uma notificação
         /// </summary>
         /// <param name="property">Nome da propriedade que está sendo testada</param>
         /// <param name="message">Mensagem personalizada</param>
-        public void AddNotification(string property, string message)
-        {
-            _notifications.Add(new Notification(property, message));
-        }
+        void AddNotification(string property, string message);
 
         /// <summary>
         /// Adiciona uma notificação
         /// </summary>
         /// <param name="notification">Objeto de notificação que deseja adicionar</param>
-        public void AddNotification(Notification notification)
-        {
-            _notifications.Add(notification);
-        }
-
+        void AddNotification(Notification notification);
         /// <summary>
         /// Adiciona uma lista de notificações
         /// </summary>
         /// <param name="notifications">Lista de notificações que deseja adicionar</param>
-        public void AddNotifications(IReadOnlyCollection<Notification> notifications)
-        {
-            _notifications.AddRange(notifications);
-        }
+        void AddNotifications(IReadOnlyCollection<Notification> notifications);
 
         /// <summary>
         /// Adiciona uma coleção de objetos notificaveis na classe principal
         /// </summary>
         /// <param name="objects">Objetos notificáveis</param>
-        public void AddNotifications(params Notifiable[] objects)
-        {
-            objects.ToList().ForEach(x => _notifications.AddRange(x.Notifications));
-        }
+        void AddNotifications(params Notifiable[] objects);
+        /// <summary>
+        /// Adiciona uma lista de notificações
+        /// </summary>
+        /// <param name="notifications">Lista de notificações que deseja adicionar</param>
+        void AddNotifications(IList<Notification> notifications);
 
         /// <summary>
         /// Adiciona uma lista de notificações
         /// </summary>
         /// <param name="notifications">Lista de notificações que deseja adicionar</param>
-        public void AddNotifications(IList<Notification> notifications)
-        {
-            _notifications.AddRange(notifications);
-        }
-
-        /// <summary>
-        /// Adiciona uma lista de notificações
-        /// </summary>
-        /// <param name="notifications">Lista de notificações que deseja adicionar</param>
-        public void AddNotifications(ICollection<Notification> notifications)
-        {
-            _notifications.AddRange(notifications);
-        }
-
+        void AddNotifications(ICollection<Notification> notifications);
         /// <summary>
         /// Verifica se o objeto notificável é valido
         /// </summary>
         /// <returns>Retornar true quando o objeto é valido, ou seja, não possui notificações.</returns>
-        public bool IsValid() => _notifications.Count == 0;
-
+        bool IsValid();
     }
 }

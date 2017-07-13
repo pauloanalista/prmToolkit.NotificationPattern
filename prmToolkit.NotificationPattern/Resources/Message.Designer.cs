8292a9475121584c162c90209baf883750d43be3
Alberto Monteiro <alberto.monteiro@live.com>
2017-07-13 13:31:46 -0300

    Usando um unico projeto que targeteia .NET Standard 1.0

--- prmToolkit.NotificationPattern/Resources/Message.Designer.cs
+++ prmToolkit.NotificationPattern/Resources/Message.Designer.cs
@@ -8,10 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System.Reflection;
-
-namespace prmToolkit.NotificationPattern.Resources
-{
+namespace prmToolkit.NotificationPattern.Resources {
+    using System;
+    using System.Reflection;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,382 +23,310 @@ namespace prmToolkit.NotificationPattern.Resources
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Message
-    {
-
+    public class Message {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Message()
-        {
+        internal Message() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if (object.ReferenceEquals(resourceMan, null))
-                {
-#if NETSTANDARD1_4
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("prmToolkit.NotificationPattern.Resources.Message", typeof(Message).GetTypeInfo().Assembly); 
-#else
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("prmToolkit.NotificationPattern.Resources.Message", typeof(Message).Assembly);
-#endif
+        public static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("prmToolkit.NotificationPattern.Resources.Message", typeof(Message).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        public static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} não deve ser igual a {1}..
         /// </summary>
-        internal static string IfAreEquals
-        {
-            get
-            {
+        public static string IfAreEquals {
+            get {
                 return ResourceManager.GetString("IfAreEquals", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ter uma instância para uma coleção..
         /// </summary>
-        internal static string IfCollectionIsNull
-        {
-            get
-            {
+        public static string IfCollectionIsNull {
+            get {
                 return ResourceManager.GetString("IfCollectionIsNull", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser uma coleção com pelo menos um item dentro.
         /// </summary>
-        internal static string IfCollectionIsNullOrEmpty
-        {
-            get
-            {
+        public static string IfCollectionIsNullOrEmpty {
+            get {
                 return ResourceManager.GetString("IfCollectionIsNullOrEmpty", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} não deve conter {1}..
         /// </summary>
-        internal static string IfContains
-        {
-            get
-            {
+        public static string IfContains {
+            get {
                 return ResourceManager.GetString("IfContains", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} não deve ser igual a zero..
         /// </summary>
-        internal static string IfEqualsZero
-        {
-            get
-            {
+        public static string IfEqualsZero {
+            get {
                 return ResourceManager.GetString("IfEqualsZero", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser verdadeiro..
         /// </summary>
-        internal static string IfFalse
-        {
-            get
-            {
+        public static string IfFalse {
+            get {
                 return ResourceManager.GetString("IfFalse", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser menor que {1}..
         /// </summary>
-        internal static string IfGreaterOrEqualsThan
-        {
-            get
-            {
+        public static string IfGreaterOrEqualsThan {
+            get {
                 return ResourceManager.GetString("IfGreaterOrEqualsThan", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser menor ou igual a {1}..
         /// </summary>
-        internal static string IfGreaterThan
-        {
-            get
-            {
+        public static string IfGreaterThan {
+            get {
                 return ResourceManager.GetString("IfGreaterThan", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ter seu comprimento menor ou igual a {1} caracter(es)..
         /// </summary>
-        internal static string IfLengthGreaterThan
-        {
-            get
-            {
+        public static string IfLengthGreaterThan {
+            get {
                 return ResourceManager.GetString("IfLengthGreaterThan", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ter seu comprimento maior ou igual a {1} caracter(es)..
         /// </summary>
-        internal static string IfLengthLowerThan
-        {
-            get
-            {
+        public static string IfLengthLowerThan {
+            get {
                 return ResourceManager.GetString("IfLengthLowerThan", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ter exatamente {1} caracter(es)..
         /// </summary>
-        internal static string IfLengthNoEqual
-        {
-            get
-            {
+        public static string IfLengthNoEqual {
+            get {
                 return ResourceManager.GetString("IfLengthNoEqual", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser maior que {1}..
         /// </summary>
-        internal static string IfLowerOrEqualsThan
-        {
-            get
-            {
+        public static string IfLowerOrEqualsThan {
+            get {
                 return ResourceManager.GetString("IfLowerOrEqualsThan", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser maior ou igual a {1}..
         /// </summary>
-        internal static string IfLowerThan
-        {
-            get
-            {
+        public static string IfLowerThan {
+            get {
                 return ResourceManager.GetString("IfLowerThan", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser igual a {1}..
         /// </summary>
-        internal static string IfNotAreEquals
-        {
-            get
-            {
+        public static string IfNotAreEquals {
+            get {
                 return ResourceManager.GetString("IfNotAreEquals", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser um Cnpj válido..
         /// </summary>
-        internal static string IfNotCnpj
-        {
-            get
-            {
+        public static string IfNotCnpj {
+            get {
                 return ResourceManager.GetString("IfNotCnpj", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve conter {1}..
         /// </summary>
-        internal static string IfNotContains
-        {
-            get
-            {
+        public static string IfNotContains {
+            get {
                 return ResourceManager.GetString("IfNotContains", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser um Cpf válido..
         /// </summary>
-        internal static string IfNotCpf
-        {
-            get
-            {
+        public static string IfNotCpf {
+            get {
                 return ResourceManager.GetString("IfNotCpf", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser uma data válida..
         /// </summary>
-        internal static string IfNotDate
-        {
-            get
-            {
+        public static string IfNotDate {
+            get {
                 return ResourceManager.GetString("IfNotDate", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser um endereço de e-mail válido..
         /// </summary>
-        internal static string IfNotEmail
-        {
-            get
-            {
+        public static string IfNotEmail {
+            get {
                 return ResourceManager.GetString("IfNotEmail", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser um Guid válido..
         /// </summary>
-        internal static string IfNotGuid
-        {
-            get
-            {
+        public static string IfNotGuid {
+            get {
                 return ResourceManager.GetString("IfNotGuid", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser igual a nulo..
         /// </summary>
-        internal static string IfNotNull
-        {
-            get
-            {
+        public static string IfNotNull {
+            get {
                 return ResourceManager.GetString("IfNotNull", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser nulo..
         /// </summary>
-        internal static string IfNotNullOrEmpty
-        {
-            get
-            {
+        public static string IfNotNullOrEmpty {
+            get {
                 return ResourceManager.GetString("IfNotNullOrEmpty", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve estar no intervalo de {1} a {2}..
         /// </summary>
-        internal static string IfNotRange
-        {
-            get
-            {
+        public static string IfNotRange {
+            get {
                 return ResourceManager.GetString("IfNotRange", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser uma URL válida..
         /// </summary>
-        internal static string IfNotUrl
-        {
-            get
-            {
+        public static string IfNotUrl {
+            get {
                 return ResourceManager.GetString("IfNotUrl", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} não deve ser igual a nulo..
         /// </summary>
-        internal static string IfNull
-        {
-            get
-            {
+        public static string IfNull {
+            get {
                 return ResourceManager.GetString("IfNull", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} é obrigatório..
         /// </summary>
-        internal static string IfNullOrEmpty
-        {
-            get
-            {
+        public static string IfNullOrEmpty {
+            get {
                 return ResourceManager.GetString("IfNullOrEmpty", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} é obrigatório e deve conter entre {1} e {2} caracteres..
         /// </summary>
-        internal static string IfNullOrInvalidLength
-        {
-            get
-            {
+        public static string IfNullOrInvalidLength {
+            get {
                 return ResourceManager.GetString("IfNullOrInvalidLength", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} é obrigatório..
         /// </summary>
-        internal static string IfNullOrWhiteSpace
-        {
-            get
-            {
+        public static string IfNullOrWhiteSpace {
+            get {
                 return ResourceManager.GetString("IfNullOrWhiteSpace", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} não deve estar no intervalo de {1} a {2}..
         /// </summary>
-        internal static string IfRange
-        {
-            get
-            {
+        public static string IfRange {
+            get {
                 return ResourceManager.GetString("IfRange", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to O campo {0} deve ser falso..
         /// </summary>
-        internal static string IfTrue
-        {
-            get
-            {
+        public static string IfTrue {
+            get {
                 return ResourceManager.GetString("IfTrue", resourceCulture);
             }
         }
